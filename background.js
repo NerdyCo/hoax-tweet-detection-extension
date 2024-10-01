74348a18e930e07ee9cfdad8c81775fa08f36204
leaDev13 <learning13062002@gmail.com>
2024-10-01 10:59:37 +0700

    fix executeScript

--- background.js
+++ background.js
@@ -1,8 +1,26 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && /x\.com\/[^\/]+\/status\/\d+/.test(tab.url)) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ["contentScript.js"],
-    });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === "complete" &&
+    tab.url &&
+    /x\.com\/[^\/]+\/status\/\d+/.test(tab.url)
+  ) {
+    const parts = tab.url.split("/");
+    const lastPart = parts[parts.length - 1];
+    let angka = 0;
+
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        files: ["contentScript.js"],
+      })
+      .then(() => {
+        console.log("Script executed successfully");
+        chrome.tabs.sendMessage(tabId, {
+          message: "TabUpdated",
+          tweetId: lastPart,
+          angka: 0,
+        });
+      })
+      .catch((error) => console.error("Script execution failed:", error));
   }
 });
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,21 @@
 (() => {
-  const contentTweet = document.querySelector('div[data-testid="tweetText"]');
-  let text = "";
-  let twitterOptions = [];
+  let currentTweet = "";
+  let currentTweetBookmarks = [];
+
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { message, tweetId } = obj;
+
+    if (message === "TabUpdated" && tweetId !== currentTweet) {
+      console.log(tweetId);
+      console.log(++obj.angka);
+
+      currentTweet = tweetId;
+
+      setTimeout(() => {
+        tweetLoaded();
+      }, 400);
+    }
+  });
 
   // Function to blur tweet content
   const blurringContent = () => {
@@ -67,20 +81,55 @@
     }
   };
 
+  const fetchBookmarks = () => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get([currentTweet], (obj) => {
+        resolve(obj[currentTweet] ? JSON.parse(obj[currentTweet]) : []);
+      });
+    });
+  };
+
   // Add hoax tweet to bookmark
-  const addHoaxTweetToBookmark = () => {
+  const addHoaxTweetToBookmark = async () => {
     const newBookmark = {
       link: window.location.href,
       isHoaxTweet: true,
     };
-    // TODO: Logic to store the bookmark
+
+    // try {
+    //   const result = chrome.storage.sync.get([currentTweet]);
+
+    //   currentTweetBookmarks = result[currentTweet]
+    //     ? JSON.parse(result[currentTweet])
+    //     : [];
+    //   currentTweetBookmarks.push(newBookmark);
+    //   currentTweetBookmarks.sort();
+
+    //   chrome.storage.sync.set({
+    //     [currentTweet]: JSON.stringify(currentTweetBookmarks),
+    //   });
+    // } catch (err) {
+    //   console.error(`Error adding bookmark: ${err}`);
+    // }
+
+    currentTweetBookmarks = await fetchBookmarks();
+    console.log(currentTweetBookmarks);
+
+    chrome.storage.sync.set({
+      [currentTweet]: JSON.stringify(
+        [...currentTweetBookmarks, newBookmark].sort()
+      ),
+    });
   };
 
   // Check the content of the tweet
   const checkingTweet = () => {
-    if (contentTweet.children.length === 1) {
+    const contentTweet = document.querySelector('div[data-testid="tweetText"]');
+    let text = "";
+
+    if (contentTweet && contentTweet.children.length === 1) {
       text = contentTweet.children[0].innerHTML;
-    } else if (contentTweet.children.length > 1) {
+    } else if (contentTweet && contentTweet.children.length > 1) {
       text = Array.from(contentTweet.children)
         .map((e) => e.textContent)
         .join(" ");
@@ -88,12 +137,16 @@
       console.log("No content found.");
     }
 
-    console.log(text);
+    // TODO: make request to turnbackhoax API with "text" as param
+
+    // TODO: make GPTModel create message response in order to show up to users
+
     blurringContent();
+    addHoaxTweetToBookmark();
   };
 
   // Add "Periksa" button when tweet is loaded
-  const tweetLoaded = async () => {
+  const tweetLoaded = () => {
     const checkBtnExists = document.getElementsByClassName("check-btn")[0];
 
     if (!checkBtnExists) {
@@ -115,6 +168,9 @@
       `;
 
       let styleSheet = document.createElement("style");
+      let twitterOptions = document.getElementsByClassName(
+        "css-175oi2r r-1awozwy r-18u37iz r-1cmwbt1 r-1wtj0ep"
+      )[0];
 
       checkBtn.textContent = "Periksa";
       checkBtn.className = "tweet-btn check-btn";
@@ -123,18 +179,12 @@
       styleSheet.textContent = styles;
       document.head.appendChild(styleSheet);
 
-      twitterOptions = document.getElementsByClassName(
-        "css-175oi2r r-1awozwy r-18u37iz r-1cmwbt1 r-1wtj0ep"
-      );
-
-      if (twitterOptions.length > 0) {
-        twitterOptions[0].insertBefore(checkBtn, twitterOptions[0].firstChild);
+      if (twitterOptions) {
+        twitterOptions.insertBefore(checkBtn, twitterOptions.firstChild);
         checkBtn.addEventListener("click", checkingTweet);
       } else {
         console.log("Element not found");
       }
     }
   };
-
-  tweetLoaded();
 })();
